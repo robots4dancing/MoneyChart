e7b7580272d235442b7b4a88e72716f839be5bd0
Valerie Greer <vagreer10@Valeries-MacBook-Pro.local>
2017-04-19 13:45:05 -0400

    Getting son from api and parsing it.

--- MoneyChart/ViewController.swift
+++ MoneyChart/ViewController.swift
@@ -9,15 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let hostName = "api.fixer.io"
+    
+    //MARK: - Data Retrieval Methods
+    
+    func parseJson(data: Data) {
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String:Any]
+            print("JSON:\(jsonResult)")
+            let currencyArray = jsonResult["rates"] as! [String:Any]
+            for (key, value) in currencyArray {
+                print("Currency:\(key):\(value)")
+            }
+                DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+        } catch {
+            print("JSON Parsing Error")
+        }
+        DispatchQueue.main.async {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+    }
+    
+    func getFile(filename: String) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let urlString = "https://\(hostName)/\(filename)"
+        print("\(urlString)")
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.timeoutInterval = 30
+        request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard let recvData = data else {
+                print("No Data")
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                return
+            }
+            if recvData.count > 0 && error == nil {
+                print("Got Data:\(recvData)")
+                let dataString = String.init(data: recvData, encoding: .utf8)
+                print("Got Data String:\(String(describing: dataString))")
+                self.parseJson(data: recvData)
+            } else {
+                print("Got Data of Length 0")
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+        }
+        task.resume()
+    }
+
+    
+    //MARK: - Life Cycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getFile(filename: "latest?base=USD")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
